550f7b2faed995d804a4659109dcf8bb6db7410d
Zachoenga <zachheroku3@gmail.com>
2024-08-01 14:19:45 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,82 @@
+<h1 align="center"> Zach-MD V1.3.3 </h1>
+<p align="center">  
+  
+***
+  
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Black+Ops+One&size=50&pause=1000&color=1BAFBAFF&center=true&width=910&height=100&lines=THANKS FOR CHOOSING ;RIAS_GREMORY-BOT;MULTI+DEVICE+WHATSAPP+BOT;CREATED+BY+TOXXIC+BOY;RELEASED+09.07.24" alt="Typing SVG" /></a>
+  </p>
+    <img alt="ZACH-MD" width="720" height="931" src="https://telegra.ph/file/1e544e25fb9217c7714b2.jpg">
+<p align="center">
+<priasgremorybot align="center">
+<a href="https://github.com/Zachoenga/Zach-MD"><img title="Author" src="https://img.shields.io/badge/Rias Gremory-black?style=for-the-badge&logo=github"></a>
+<p align="center">
+<a href="https://github.com/Zachoenga/followers"><img title="Followers" src="https://img.shields.io/github/followers/Zachoenga?color=blue&style=flat-square"></a>
+<a href="https://github.com/Zachoenga/Zach-MD/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/Zachoenga/Zach-MD?color=red&style=flat-square"></a>
+<a href="https://github.com/Zachoenga/Zach-MD/network/members"><img title="Forks" src="https://img.shields.io/github/forks/Zachoenga/Zach-MD?color=green&style=flat-square"></a>
+<a href="https://github.com/Zachoenga/Zach-MD/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/Zachoenga/Zach-MD?label=Watchers&color=yellow&style=flat-square"></a>
+
+#### SETUP 
+
+### 1.`First STAR 🌟 This Repo ` And Then [`FORK`](https://github.com/Zachoenga/Zach-MD/fork) It
+
+### 2. GET SESSION ID HERE 
+
+<a href='https://riasgremorybot-xcqv.onrender.com/' target="_blank"><img alt='PAIR CODE' src='https://img.shields.io/badge/Click here to get your session id-blue?style=for-the-badge&logo=opencv&logoColor=white'/></a> 
+
+
+### STEP 2
+ADD SESSION ID TO YOUR FORKED REPO IN CONFIG.JS
+AND DEPLOY
+
+### DEPLOY ON SCALINGO
+
+1. If you don't have an account in Scalingo, create one and deploy.
+    <br>
+    <a href='https://auth.scalingo.com/users/sign_in' target="_blank"><img alt='REPLIT' src='https://img.shields.io/badge/-DEPLOY-orange?style=for-the-badge&logo=scalingo&logoColor=white'/></a>
+
+
+### DEPLOY ON REPLIT
+IF YOU DON'T HAVE A REPLIT ACCOUNT CREATE ONE AND DEPLOY 
+    <br>
+    <a href='https://github.com/Zachoenga/Zach-MD' target="_blank"><img alt='Replit' src='https://img.shields.io/badge/-Deploy-red?style=for-the-badge&logo=replit&logoColor=white'/></a>
+    
+ ### DEPLOY ON RAILWAY 
+1. Deploy.
+    <br>
+    <a href='https://github.com/Zachoenga/Zach-MD' target="_blank"><img alt='Railway' src='https://img.shields.io/badge/-Deploy-green?style=for-the-badge&logo=render&logoColor=white'/></a>
+
+ ### DEPLOY ON RENDER 
+1. Deploy.
+    <br>
+    <a href='https://dashboard.render.com/web/new' target="_blank"><img alt='Render' src='https://img.shields.io/badge/-Deploy-grey?style=for-the-badge&logo=railway&logoColor=white'/></a>
+
+***
+
+
+### CONTRIBUTIONS 
+-Contributions to Zach-MD are welcome! If you have ideas for new features, improvements, or bug fixes, feel free to open an issue or submit a pull request.
+
+<br>
+    <a href='https://github.com/Zachoenga/Zach-MD/issues/new/choose' target="_blank"><img alt='Railway' src='https://img.shields.io/badge/-REPORT ISSUE-red?style=for-the-badge&logo=railway&logoColor=white'/></a>
+
+
+***
+
+### LICENSE 
+-The Zach-MD is released under the [Apache 2.0 License](https://opensource.org/licenses/MIT).
+
+-Enjoy the diverse features of the Zach-MD to enhance your conversations and make your WhatsApp experience more interesting!
+
+***
+### DEVELOPER:
+**GET In Touch with The Owner**
+- [**On TELEGRAM**](https://t.me/ZachMjeshi)
+
+***
+
+### THANKS TO:
+
+- 
+- 
+- DAVID CRYIL FOR INSPIRATION
+
---
+++ config.js
@@ -0,0 +1,77 @@
+//#ZACH
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+global.audio = "";
+global.video = "";
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "obidikechikadibia@gmail.com";
+global.location = "Ondo, Nigeria";
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://astrofx0011:astro@cluster0.lmwnxdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Nairobi";
+global.github = process.env.GITHUB || "https://github.com/Zachoenga/Zach-MD";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vaex2BtGU3BRQoeEsl0U";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vaex2BtGU3BRQoeEsl0U";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/1e544e25fb9217c7714b2.jpg";
+global.devs = "254726657531";
+global.sudo = process.env.SUDO || "254726657531";
+global.owner = process.env.OWNER_NUMBER || "254726657531";
+global.style = process.env.STYLE || "1";
+global.gdbye = process.env.GOODBYE || "false";
+global.wlcm = process.env.WELCOME || "false";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
+global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/1e544e25fb9217c7714b2.jpg";
+global.waPresence = process.env.WAPRESENCE || "alwaysonline";
+global.readcmds = process.env.READ_COMMAND || "false";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "";
+global.read_status_from = process.env.READ_STATUS_FROM || "";
+
+global.api_smd = "https://toxic-session-id.onrender.com/pair";
+global.scan = "https://riasgremorybot-xcqv.onrender.com/";
+
+global.SESSION_ID =
+  process.env.SESSION_ID ||
+  ""
+module.exports = {
+  menu: process.env.MENU || "1",
+  HANDLERS: process.env.PREFIX || ".",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.3.1",
+  caption: process.env.CAPTION || "𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 ZACH",
+  author: process.env.PACK_AUTHER || "ZACH ᴍᴅ",
+  packname: process.env.PACK_NAME || "ZACH 𝗠𝗗",
+  botname: process.env.BOT_NAME || "Zach-MD",
+  ownername: process.env.OWNER_NAME || "Zach",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "neLbXXrp8bSDcohnp1CW5UEa",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "sk-proj-pBksWDImP6VuvvJTvqDsT3BlbkFJ2E8GhbpzjyRFkFaBI9Aj",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "HRKU-2fc4d785-535b-4113-8291-84c5d3d6d2c3",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "toxicmd12",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  LANG: (process.env.THEME || "Zach").toUpperCase(),
+};
+global.rank = "updated";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});
